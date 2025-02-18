8a056ff856db601db84bc55c3a1a4ac51fe281ac
itskleb <itskleb@gmail.com>
2025-02-18 13:58:13 -0500

    Update vov_analysis.py

--- vov_analysis.py
+++ vov_analysis.py
@@ -68,9 +68,8 @@ for t in topic:
   if t in ['de_purp','camp','pain']:
     st.write(f"Total Respondents: {len(temp2)}. That is {round((len(temp2)/len(df))*100,2)}% of total.")
     st.write(rankers(temp2,t))
-    charty, ax = plt.subplots()
-    ax.pie(rankers(temp2,t))
-    st.pyplot(charty)
+    charty = px.bar(rankers(temp2,t)
+    st.plotly_chart(charty)
   else:
     temp = temp2.groupby(by=[t,'program']).count()['sub_date'].sort_values(ascending=False)
     st.write(f"Total Respondents: {len(temp2)}. That is {round((len(temp2)/len(df))*100,2)}% of total.")

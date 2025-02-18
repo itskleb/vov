839aec52c82283f9e65bff4a75b1c142a356766f
itskleb <itskleb@gmail.com>
2025-02-18 12:43:43 -0500

    Update vov_analysis.py

--- vov_analysis.py
+++ vov_analysis.py
@@ -64,7 +64,7 @@ topic = st.multiselect('Select Question Topic',df.columns.tolist(),None)
 temp = df[df['program'].isin(prgm) & df['length'].isin(lgth) & df['youth'].isin(youth) & df['district_vol'].isin(dist) & df['eagle'].isin(eagle)]
 for t in topic:
   if t in ['de_purp','camp','pain']:
-    st.write(rankers(temp,t)
+    st.write(rankers(temp,t))
   else:
     temp = df[df['program'].isin(prgm) & df['length'].isin(lgth) & df['youth'].isin(youth) & df['district_vol'].isin(dist) & df['eagle'].isin(eagle)].groupby(by=t).count()['sub_date']
     st.bar_chart(data=temp,  horizontal=True, )

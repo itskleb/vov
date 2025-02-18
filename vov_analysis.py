2af4287cfbd19bd63d945aa576057e2639621384
itskleb <itskleb@gmail.com>
2025-02-18 12:59:55 -0500

    Update vov_analysis.py

--- vov_analysis.py
+++ vov_analysis.py
@@ -66,5 +66,5 @@ for t in topic:
   if t in ['de_purp','camp','pain']:
     st.write(rankers(temp,t))
   else:
-    temp = df[df['program'].isin(prgm) & df['length'].isin(lgth) & df['youth'].isin(youth) & df['district_vol'].isin(dist) & df['eagle'].isin(eagle)].groupby(by=t).count()['sub_date'].sort_values(ascending=True)
-    st.bar_chart(data=temp,  horizontal=True, )
+    temp = df[df['program'].isin(prgm) & df['length'].isin(lgth) & df['youth'].isin(youth) & df['district_vol'].isin(dist) & df['eagle'].isin(eagle)].groupby(by=t).count()['sub_date'].sort_values(ascending=False)
+    st.bar_chart(data=temp,  horizontal=False)
